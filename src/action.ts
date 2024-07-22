9894c2a06821db83b37a0ba327242d14f115a65f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-07-22 00:38:42 -0300

    fix: modify CreateAction to persist meta and context

--- src/action.ts
+++ src/action.ts
@@ -6,18 +6,28 @@ type ContextFn = () => MaybePromise<object>
 
 type Unwrap<T> = T extends () => Promise<infer U> ? Awaited<U> : T extends () => infer U ? U : T
 
+type CreateActionBuilderProps = {
+	meta?: object
+	context?: ContextFn
+}
+
 class CreateActionBuilder<Context, Meta> {
 	#meta: object | undefined = undefined
 	#context: ContextFn | undefined = undefined
 
+	constructor(opts?: CreateActionBuilderProps) {
+		this.#meta = opts?.meta
+		this.#context = opts?.context
+	}
+
 	meta<NewMeta extends object>(meta: NewMeta) {
 		this.#meta = meta
-		return new CreateActionBuilder<Context, NewMeta>()
+		return new CreateActionBuilder<Context, NewMeta>({ meta, context: this.#context })
 	}
 
 	context<NewContext extends ContextFn>(ctx: NewContext) {
 		this.#context = ctx
-		return new CreateActionBuilder<NewContext, Meta>()
+		return new CreateActionBuilder<NewContext, Meta>({ meta: this.#meta, context: ctx })
 	}
 
 	create(opts?: {
